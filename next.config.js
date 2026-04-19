f0d83521023f67d7f7f0243d5e555e219f3a9c8c
andrew@asc-solutions.co.uk <andrew@asc-solutions.co.uk>
2026-04-19 21:22:38 +0100

    update func

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['@azure/data-tables', '@azure/identity'],
 }
 
 module.exports = nextConfig
